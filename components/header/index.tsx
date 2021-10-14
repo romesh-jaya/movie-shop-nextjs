7818e9d5aca5969e56437ebb6a4786e69a0145cf
romesh-jaya <56665179+romesh-jaya@users.noreply.github.com>
2021-10-14 10:29:26 +0530

    feat: header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -8,23 +8,21 @@ import logo from '../../assets/images/logo.png'
 export default function Header() {
   // const [showDrawer, setShowDrawer] = useState(false)
 
-
-
   return (
     <div className={styles.container}>
       <div className={styles['inner-container']}>
-      <Link href={'/'}>
-        <div className={styles.logo}>
-          <Image
-            src={logo}
-            alt='Ultra logo'
-            priority
-            width='165px'
-            height='72px'
-            unoptimized
-          />
-        </div>
-      </Link>
+        <Link href={'/'} passHref>
+          <div className={styles.logo}>
+            <Image
+              src={logo}
+              alt='Ultra logo'
+              priority
+              width='165px'
+              height='72px'
+              unoptimized
+            />
+          </div>
+        </Link>
       </div>
     </div>
   )
