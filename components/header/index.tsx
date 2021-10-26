0347849c90f47d61935a41d1dddb3541759d4223
romesh-jaya <56665179+romesh-jaya@users.noreply.github.com>
2021-10-26 11:46:15 +0530

    feat: links in header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,10 +4,37 @@ import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../assets/images/logo.png'
 import Query from '../query'
+import { MovieType } from '../../enums/MovieType'
+import { useRouter } from 'next/router'
 // import { Drawer, Button } from 'antd'
 
+const menuLinks = [
+  { key: 'Movies', link: `/search?type=${MovieType.Movie}` },
+  { key: 'TV Series', link: `/search?type=${MovieType.TvSeries}` },
+]
+
 export default function Header() {
+  const router = useRouter()
   // const [showDrawer, setShowDrawer] = useState(false)
+
+  const renderLinksDesktop = () => {
+    return (
+      <ul className={styles.links}>
+        {menuLinks.map(link => {
+          return (
+            <li key={link.key}>
+              <Link href={link.link}>
+                <a
+                  className={router.pathname == link.link ? 'active-link' : ''}>
+                  {link.key}
+                </a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
 
   return (
     <div className={styles.container}>
@@ -24,6 +51,7 @@ export default function Header() {
             />
           </div>
         </Link>
+        <div className={styles['links-container']}>{renderLinksDesktop()}</div>
         <span className={styles.query}>
           <Query />
         </span>
